0f14974d35101d865125c013bb2de8f203e5319c
iishikaydv <yishika308@gmail.com>
2022-02-11 17:19:07 +0530

    datatypes and strings done

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ a = null;
 console.log(a);
 console.log(typeof a);
 console.log();
+
+//numbervar 
+num = 10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+var t = true;
+var f = false;
+console.log(t, f);
+
+//string-> in this we can use "", '', ``
+// single quotes, double quotes and backticks
+var str = 'a';
+console.log(typeof str);
+str = "Hello How You doin? \ni am fine , How about you?";
+// backslash n("\n")
+console.log(str);
+
+var b = `hi hope you guys are 
+able to grasp`;
+console.log(b);
+
+var num = 100;
+console.log("Half of 100 is 50");
+
+var num1 = 200;
+console.log(`Half of ${num1} is ${num1/2}`);
+
