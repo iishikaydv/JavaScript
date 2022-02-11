24523b4cdf3f512f235aaf5acde73bd681719053
iishikaydv <yishika308@gmail.com>
2022-02-11 17:15:03 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// starting JS-> JavaScript
+// var let const
+
+// variable declaration
+var a; // we dont have to mention what datatype it is
+
+//variable initialization
+a = 10;
+console.log(a); //for printing console.log();
+console.log(typeof a);
+
+a = "hello";
+console.log(a);
+
+//typeof operator-> it tells what is the datatype of the particular variable
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+console.log();
