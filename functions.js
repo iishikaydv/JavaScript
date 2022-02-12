53ba1f489c5e8f7bacc7c5c4cb3168fa448753fe
iishikaydv <yishika308@gmail.com>
2022-02-12 23:30:19 +0530

    Calculator Done!!!

--- functions.js
+++ functions.js
@@ -27,18 +27,35 @@ add(2, 6);*/
 // -> Functions can be returned
 // -> Functions can be passed as parameters/arguments
 
-/*function calculator(str,a ,b){
+/*function calculator(str, a ,b){
     if (str == 'add') {
-        return function add(){
-            console.log(a + b);
+        return a + b;//function add(){
+            //console.log(a + b);
+        //}
+    }
+    else if (str == 'sub'){
+        return function sub(){
+            console.log(a - b);
         }
     }
-    //else if (str == 'sub'){
-
-    //}
+    else if (str == 'mul'){
+        return function mul(){
+            console.log(a * b);
+        }
+    }
+    else if (str == 'div'){
+        return function div(){
+            console.log(a / b);
+        }
+    }
 }
+//calculator(2,3)
+console.log(calculator('add', 2,3));
+console.log(calculator('sub', 2,3));
+console.log(calculator('mul', 2,3));
+console.log(calculator('div', 2,3));*/
 
-let returnedFunc = calculator('add', 2, 3);  //whole function is returning
+/*let returnedFunc = calculator('add', 2, 3);  //whole function is returning
 console.log("returned function is\n " + returnedFunc);
 returnedFunc();*/
 
@@ -61,11 +78,11 @@ console.log("" + sayHi);*/
 // 3) IIFE -> Immediately invoked function expression
 // jese h define kro vese h call ho jaye vps call na krna pde vo functions hote h IIFE
 
-function add(a, b){
+/*function add(a, b){
     return a + b;
 }
 add(2, 3)
 let addIIFE = (function add(a, b){
     return a + b;
 })(20, 30);  // function invoked 
-console.log("" + addIIFE);
+console.log("" + addIIFE);*/
