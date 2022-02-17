eb5fd6936d24b78d5c1625695e3d4a5d7e33783a
iishikaydv <yishika308@gmail.com>
2022-02-17 13:43:47 +0530

    Slice Method Done

--- strings.js
+++ strings.js
@@ -1,5 +1,7 @@
 // Strings are sequence of characters
 var str = "How You Doin'???";
+// H o w <space> Y o u <space> D o i  n  '  ?  ?  ?
+// 0 1 2    3    4 5 6    7    8 9 10 11 12 13 14 15
 console.log(str);
 
 // printing a character
@@ -23,3 +25,18 @@ console.log(str[15]);
 // String Length
 console.log(str.length);
 
+// Strings Methods
+
+// 1) Extraction Method  -> 
+// Slice Method
+// Slice(start, end)  ->  Start index is included and end index is excluded
+
+var slicedStr = str.slice(4, 9);  // 4th index se shuru hoga aur 8th index tk string print hogi
+console.log(slicedStr);
+console.log(str);   // Original source of information is not changed
+
+var slicedStr = str.slice(4);   // Start se lekr pori end tk string print hogi
+console.log(slicedStr);
+
+var slicedStr = str.slice(2, -4);  // Start se lekr peeche k end times index chod do
+console.log(slicedStr);
