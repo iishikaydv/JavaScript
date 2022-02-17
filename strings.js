2b5ce7f159a50947c1951fb40033bc9377c06c1c
iishikaydv <yishika308@gmail.com>
2022-02-17 12:58:28 +0530

    Strings Length Done

--- strings.js
+++ strings.js
@@ -1,3 +1,25 @@
 // Strings are sequence of characters
 var str = "How You Doin'???";
 console.log(str);
+
+// printing a character
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+console.log(str[5]);
+console.log(str[6]);
+console.log(str[7]);
+console.log(str[8]);
+console.log(str[9]);
+console.log(str[10]);
+console.log(str[11]);
+console.log(str[12]);
+console.log(str[13]);
+console.log(str[14]);
+console.log(str[15]);
+
+// String Length
+console.log(str.length);
+
