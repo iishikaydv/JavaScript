58f8853a432603b1f35bd3364c792670d7e2e326
iishikaydv <yishika308@gmail.com>
2022-02-19 13:26:32 +0530

    Methods

--- objects.js
+++ objects.js
@@ -18,3 +18,24 @@ console.log(`Hey ${person.name} Thank you for signing in!!!`);
 var str = "Hello";
 console.log(str.length);    // dot(.) notation to access properties
 console.log(str["length"]);    // square bracket([]) notation to access properties
+
+// Nesting of Objects
+let captainAmerica = {
+    firstName: "Steve",
+    lastName: "Rogers",
+    friends:["Bucky","Tony Stark","Bruce Banner"],
+    age:102,
+    isAvenger: true,
+    address: {
+        state:"Manhattan",
+        city:"New York",
+        country:"USA",
+    },
+    sayHi: function(){
+        console.log(`Hello My name is ${this.firstName}`);
+    }
+}
+console.log(captainAmerica);
+
+// Methods  ->  jo functions objects k andar bne ho unhe methods kehte h
+// Jo bhi strings bnti h vo ek type se object h hoti h
