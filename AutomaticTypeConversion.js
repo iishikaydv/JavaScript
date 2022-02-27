83c7d04cb6a91316a2e641584889c483c545a6fc
iishikaydv <yishika308@gmail.com>
2022-02-27 18:38:33 +0530

    Type Conversion of Integer into String!!!

---
+++ AutomaticTypeConversion.js
@@ -0,0 +1,6 @@
+var ans = "5" + 1;  // "5" + "1" -> 51  Precedence of String is more than Integer
+// var ans = "Chiki" + 1; // "Chiki" + "1" -> Chiki1 (1 is converted into String type)
+console.log(ans);
+
+var ans = 1 + "5";  //"1" + "5" -> 15
+console.log(ans);  // bod>m>a>s  ->  brackets open division multiplication addition substraction
