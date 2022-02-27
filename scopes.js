de890ef9253d2df3c52aff1ec49a8d626b8b3be5
iishikaydv <yishika308@gmail.com>
2022-02-27 20:42:07 +0530

    Scope LexicalEnvironment and ScopeChaining

--- scopes.js
+++ scopes.js
@@ -7,3 +7,9 @@ function outer() {
 var b = 10;
 outer();
 console.log(b);
+
+// Scope  -> Where can this variable/ Function be accessed in the code
+//                        OR
+// is variable/ function inside the scope of code/function
+// Lexical environment = local memory + lexical environment of parent
+// Scope chaining -> chain of lexical environment and the parent references
