4078df09b78420da8c428e5b1f9c4bdf7f5541f0
iishikaydv <yishika308@gmail.com>
2022-02-27 18:57:46 +0530

    Automatic Type Conversion Done!!!

--- AutomaticTypeConversion.js
+++ AutomaticTypeConversion.js
@@ -2,5 +2,16 @@ var ans = "5" + 1;  // "5" + "1" -> 51  Precedence of String is more than Intege
 // var ans = "Chiki" + 1; // "Chiki" + "1" -> Chiki1 (1 is converted into String type)
 console.log(ans);
 
-var ans = 1 + "5";  //"1" + "5" -> 15
+var ans = 1 + "5";  //"1" + "5" -> 15  // + sign stands for addition and concatenation
 console.log(ans);  // bod>m>a>s  ->  brackets open division multiplication addition substraction
+
+console.log(null * 5);  // output: 0 (Null is converted into 0)  //Default of javascript's design  ->  Null and Undefined are almost same
+console.log(null * "5");
+
+console.log(undefined * 5);  //Output: NaN
+
+console.log("5" - 1);  // Output: 4  // converts strings into integers 
+
+console.log("ten" * 3);   //Nan ("ten" string cannot be mapped into a number like "5" can be mapped to a number)
+
+console.log("10" * 3);
