9142c6e96f19653925f705d4ad2dd19432f55141
iishikaydv <yishika308@gmail.com>
2022-02-27 18:17:50 +0530

    Execution Completed

--- execution.js
+++ execution.js
@@ -4,5 +4,8 @@ function cube(num){
 }
 var cube1 = cube(num);
 var cube2 = cube(10);
+var cube3 = cube();  
 console.log(cube1);
 console.log(cube2);
+console.log(cube3);  // Error : NaN (Not a Number, Non sensible calculation)
+console.log(undefined * 5 * undefined);
