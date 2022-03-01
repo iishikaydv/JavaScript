4cd157bdaca840c1a6beeae64c0446fc7520587c
iishikaydv <yishika308@gmail.com>
2022-03-01 11:15:28 +0530

    Difference between = , == and ===

--- tests.js
+++ tests.js
@@ -1,8 +1,16 @@
-let randomValue = {name:"Lydia"};
+//Question-1
+/*let randomValue = {name:"Lydia"};
 randomValue = 23;
 
 if(!typeof randomValue === "strings") {
     console.log("It's not a string!!!");
 } else {
     console.log("Yay It's a string!!!");
+}*/
+
+var a = 10;  // Assign Values
+if(a == 11){  // Checks Values
+    console.log("Ishika");
+} else if(a === 11){  // Firstly Checks Typeof and then Checks Value
+    console.log("Chiki");
 }
