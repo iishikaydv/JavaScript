48b061fd1cc61f3dbb8462c9c8f10ef8dcf4040e
iishikaydv <yishika308@gmail.com>
2022-03-01 12:38:20 +0530

    Question-4 Done!!!

--- tests.js
+++ tests.js
@@ -27,11 +27,20 @@ user.updateEmail("new@email.com");
 console.log(user.email);*/  // This will be updated email  
 
 //Question-3
-const fruit = ["Banana","Orange","Apple"];
+/*const fruit = ["Banana","Orange","Apple"];
 
 fruit.slice(0, 1);  // Kha se kha tk element delete krna h (startIdx,num_of_elements_delete)
 // Does not change original value of arrays
 fruit.splice(0, 1);  // ye original arrays m change krega
 fruit.unshift("Cherry");
 
-console.log(fruit);
+console.log(fruit);*/
+
+//Question-4
+let count = 0;
+const nums = [0, 1, 2, 3];
+for(var i in nums){
+    if (i) count = count + 1;
+}
+
+console.log(count);
