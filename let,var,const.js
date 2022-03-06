1b284b79671dcc37dc9e37ff3ea64e0e743454b8
iishikaydv <yishika308@gmail.com>
2022-03-06 17:13:55 +0530

    let block scoped

--- let,var,const.js
+++ let,var,const.js
@@ -1,7 +1,7 @@
-console.log(x);
+/*console.log(x);
 var x = 10;
 let b = 100;    // This has to be initialised first
-console.log(b);
+console.log(b);*/ 
 
 // Blocks  ->  Those statements which comes between these curly braces are termed as blocks.
 // It is used to compound statements.
@@ -9,6 +9,23 @@ console.log(b);
 
 // }
 
-if(10>0){
-    // Performs all statements under these curly brackets.
+// if(10>0){
+//     // Performs all statements under these curly brackets.
+// }
+
+// Block Scope  ->  Variables which are declared in a block are accessible inside that block, those variables are known to be block scoped.
+// Variables declared using let and const are block scoped.
+// var is function scoped
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
+console.log(a);
+console.log(b);
+//console.log(c);
