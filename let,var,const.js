022c6a46a135130add7ab09c1fce3e36f0e2405e
iishikaydv <yishika308@gmail.com>
2022-03-06 17:03:57 +0530

    Blocks Example

--- let,var,const.js
+++ let,var,const.js
@@ -3,4 +3,12 @@ var x = 10;
 let b = 100;    // This has to be initialised first
 console.log(b);
 
-// Blocks
+// Blocks  ->  Those statements which comes between these curly braces are termed as blocks.
+// It is used to compound statements.
+// {
+
+// }
+
+if(10>0){
+    // Performs all statements under these curly brackets.
+}
