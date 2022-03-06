3c3ab5bb12bf96e91fffdf6062aa5b9ed8255b3c
iishikaydv <yishika308@gmail.com>
2022-03-06 17:26:04 +0530

    Shadowing

--- let,var,const.js
+++ let,var,const.js
@@ -17,6 +17,7 @@ console.log(b);*/
 // Variables declared using let and const are block scoped.
 // var is function scoped
 
+let b = 100;
 {
     var a = 10;
     let b = 20;
@@ -26,6 +27,7 @@ console.log(b);*/
     console.log(b);
     console.log(c);
 }
-console.log(a);
-//console.log(b);
-console.log(c);
+// console.log(a);
+// //console.log(b);
+// console.log(c);
+console.log(b);  // Shadowing  ->  When a variable is declared in a certain scope having the same name defined on its outer scope and when we call the variable from the inner scope, the value assigned to the variable in the inner scope is the value that will be stored in the variable in the memory space. This is known as Shadowing or Variable Shadowing. In JavaScript, the introduction of let and const in ECMAScript 6 along with block scoping allows variable shadowing.
