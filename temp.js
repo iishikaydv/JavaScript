f8f4795a845513d828bb63c90ddfa6a8ae49cec1
iishikaydv <yishika308@gmail.com>
2022-03-09 21:44:00 +0530

    temp file created for node js

---
+++ temp.js
@@ -0,0 +1,14 @@
+var obj = {
+    add:function add(a, b) {
+        return a + b;
+    },
+
+    sub:function substract(a, b) {
+        return a - b;
+    },
+    hello:123
+}
+
+console.log(obj);
+
+module.exports = obj;
